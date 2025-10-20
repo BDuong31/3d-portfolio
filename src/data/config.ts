1578b5df4ec91b7a82dfdfca5f850d3a0a29ef3b
BDuong31 <2331540042@vaa.edu.vn>
2025-10-20 16:34:38 +0700

    first commit

---
+++ src/data/config.ts
@@ -0,0 +1,41 @@
+const config = {
+  title: "Vu Thai Binh Duong | Full-Stack Developer",
+  description: {
+    long: "Explore the portfolio of Vu Thai Binh Duong, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
+    short:
+      "Discover the portfolio of Vu Thai Binh Duong, a full-stack developer creating interactive web experiences and innovative projects.",
+  },
+  keywords: [
+    "Binh Duong",
+    "portfolio",
+    "full-stack developer",
+    "creative technologist",
+    "web development",
+    "3D animations",
+    "interactive websites",
+    "Coding Ducks",
+    "The Booking Desk",
+    "Ghostchat",
+    "web design",
+    "GSAP",
+    "React",
+    "Next.js",
+    "Spline",
+    "Framer Motion",
+  ],
+  author: "Vu Thai Binh Duong",
+  email: "binhduong31725@gmail.com",
+  site: "https://vubinhduong.site",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/vubinhduong",
+    linkedin: "https://www.linkedin.com/in/vubinhduong/",
+    instagram: "https://www.instagram.com/bduuong/",
+    facebook: "https://www.facebook.com/BDuonng/",
+    github: "https://github.com/BDuong31",
+  },
+};
+export { config };
